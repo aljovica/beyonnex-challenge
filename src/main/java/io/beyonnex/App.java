f5da542274d116af3ba55b0731e41596baacd7c8
Azer Aljovic <azeraljovic@Azers-Air.fritz.box>
2024-03-20 07:13:53 +0100

    remove recursion

--- src/main/java/io/beyonnex/App.java
+++ src/main/java/io/beyonnex/App.java
@@ -14,31 +14,29 @@ public class App {
 
         Scanner scanner = new Scanner(System.in);
         DependencyConfig dependencyConfig = new DependencyConfig();
-        processCommand(scanner, dependencyConfig);
 
-        scanner.close();
-    }
-
-    private static void processCommand(Scanner scanner, DependencyConfig dependencyConfig) {
         var featureOneService = dependencyConfig.getFeatureOneService();
         var featureTwoService = dependencyConfig.getFeatureTwoService();
 
-        out.print("Enter a command (type 'exit' to quit or 'help' for list of commands): ");
-        String inputCommand = scanner.nextLine().toLowerCase();
+        boolean isRunning = true;
+        while (isRunning) {
+            out.print("Enter a command (type 'exit' to quit or 'help' for list of commands): ");
+            String inputCommand = scanner.nextLine().toLowerCase();
 
-        switch (CommandParser.parse(inputCommand)) {
-            case F1Command(String firstText, String secondText) -> out.println(featureOneService.execute(firstText, secondText));
-            case F2Command(String text) -> out.println(featureTwoService.execute(text));
-            case HelpCommand() -> getHelpMessage();
-            case UnknownCommand(String errorMessage) -> out.println(errorMessage);
-            case ExitCommand() -> {
-                out.println("Exiting the application. Goodbye!");
-                return;
+            switch (CommandParser.parse(inputCommand)) {
+                case F1Command(String firstText, String secondText) -> out.println(featureOneService.execute(firstText, secondText));
+                case F2Command(String text) -> out.println(featureTwoService.execute(text));
+                case HelpCommand() -> getHelpMessage();
+                case UnknownCommand(String errorMessage) -> out.println(errorMessage);
+                case ExitCommand() -> {
+                    out.println("Exiting the application. Goodbye!");
+                    isRunning = false;
+                }
+                default -> {} // compiler is not smart enough
             }
-            default -> {} // compiler is not smart enough
         }
 
-        processCommand(scanner, dependencyConfig);
+        scanner.close();
     }
 
     private static void getHelpMessage() {
@@ -48,5 +46,4 @@ public class App {
                 2. f2("A") - Finds all anagrams of string A that were previously entered through f1
                 3. exit - Quit the application""");
     }
-
 }
